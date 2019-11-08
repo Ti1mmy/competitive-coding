859c98ef33d57fc09e0432f3a95d667d1777040d
Ti1mmy <47431121+Ti1mmy@users.noreply.github.com>
2019-11-07 22:55:13 -0500

    woo

--- Nov1.py
+++ Nov1.py
@@ -1,14 +1,16 @@
+import sys
+input = sys.stdin.readline
 mass = []
-masssum = [0]
-tmass = []
+masssum = []
 for i in range(int(input())):
     mass.append(int(input()))
-for i in range(len(mass)):
-    masssum.append(masssum[i] + mass[i])
+masssum.append(mass[0])
+for i in range(1, len(mass)):
+    masssum.append(masssum[i-1] + mass[i])
 for i in range(int(input())):
     a = input()
-    a.split()
-    print(a)
-    tmass.append(masssum[int(a[2])] - masssum[(a[0])])
-for i in range(len(tmass)):
-    print(tmass[i])
+    b = a.split()
+    if b[0] == '0':
+        print(masssum[int(b[1])])
+    else:
+        print(masssum[int(b[1])] - masssum[int(b[0])-1])
---
+++ test.py
@@ -0,0 +1,6 @@
+import sys
+input = sys.stdin.readline
+a = 'a b c d e f g'
+b = a.split()
+for i in b:
+    print(i)
